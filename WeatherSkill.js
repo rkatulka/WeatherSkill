2cbe4c7741537dbfa0851a28a0adafc5e378456d
rkatulka <bobby.katulka@gmail.com>
2016-07-16 19:18:17 -0400

    Testing the require with http, async and config file

--- WeatherSkill.js
+++ WeatherSkill.js
@@ -1,9 +1,9 @@
 var async = require('async');
 var http = require('http');
-var config = require('./config.js');
-
+var config = require('./config');
 
 exports.handler = function(event, context) {
+  console.log(event);
   console.log(config);
   console.log('Weather Skill');
   context.done();
