0e62bcc2af3af62132556036bc9e111cd0625b54
Robert Katulka <robert@robert-HP-ENVY-m6-Notebook-PC.(none)>
2016-07-16 22:30:51 -0400

    Call the Dark Sky Api and print out the result to the console.

--- WeatherSkill.js
+++ WeatherSkill.js
@@ -1,10 +1,35 @@
 var async = require('async');
-var http = require('http');
+var https = require('https');
 var config = require('./config');
 
+var darkSkyHost = config.apiHost;
+var darkSkyApiKey = config.apiKey;
+var richmondCoordinates = config.richmond;
+var fullApiPath = '/forecast/' + darkSkyApiKey + '/' + richmondCoordinates.latitude + ',' + richmondCoordinates.longitude;
+
+var requestHourlyForcast = function(callback) {
+  var options = {
+    host: darkSkyHost,
+    path: fullApiPath,
+    port: 443
+  }
+  console.log('here');
+  var req = https.request(options, function(resp) {
+    var data = '';
+    resp.on('data', function(d) {
+      data += d;
+    })
+    resp.on('end', function() {
+      console.log('data: ');
+      callback(data);
+    })
+  });
+  req.end();
+}
+
 exports.handler = function(event, context) {
-  console.log(event);
-  console.log(config);
-  console.log('Weather Skill');
-  context.done();
+  requestHourlyForcast(function(result) {
+    console.log(result);
+    context.done();
+  });
 }
--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 var config = {};
 
 config.apiKey = '3a0920f2a31a0bf03e169c15dc5e3296';
-config.apiUrl = 'api.forecast.io/'
+config.apiHost = 'api.forecast.io';
+config.richmond = {};
+config.richmond.latitude = '37.5407';
+config.richmond.longitude = '-77.4360';
 
-modules.exports = config;
+module.exports = config;
